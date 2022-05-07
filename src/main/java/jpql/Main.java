aaff0d1444733c555834f6d64c2481781c7ba850
ttasjwi <ttasjwi920@gmail.com>
2022-05-07 21:18:21 +0900

    프로젝션으로 여러값 조회하기

--- src/main/java/jpql/Main.java
+++ src/main/java/jpql/Main.java
@@ -13,20 +13,26 @@ public class Main {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("ttasjwi");
-            member.setAge(10);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setName("ttasjwi");
+            member1.setAge(10);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setName("honux");
+            member2.setAge(20);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String jpql = "SELECT m FROM Member as m WHERE m.name = :memberName";
-            Member findMember = em.createQuery(jpql, Member.class)
-                    .setParameter("memberName", member.getName())
-                    .getSingleResult();
-
-            System.out.println("findMember.getName() = " + findMember.getName());
+            String jpql = "SELECT new jpql.MemberDTO(m.name, m.age) FROM Member as m";
+            List<MemberDTO> memberDTOs = em.createQuery(jpql, MemberDTO.class)
+                    .getResultList();
+            for (MemberDTO memberDTO : memberDTOs) {
+                System.out.println("memberDTO.name = " + memberDTO.getName());
+                System.out.println("memberDTO.age = " + memberDTO.getAge());
+            }
 
             tx.commit();
         } catch(Exception e) {
