052b2c3038413121d7a5e7a02314b271fa078f65
ttasjwi <ttasjwi920@gmail.com>
2022-05-09 11:51:13 +0900

    JPQL 조건식

--- src/main/java/jpql/Main.java
+++ src/main/java/jpql/Main.java
@@ -1,6 +1,9 @@
 package jpql;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class Main {
@@ -13,25 +16,37 @@ public class Main {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("ttasjwi");
-            member.setMemberType(MemberType.ADMIN);
-            em.persist(member);
+            Member memberA = new Member();
+            memberA.setName(null);
+            memberA.setAge(20);
+            em.persist(memberA);
 
-            Book book = new Book();
-            book.setName("토끼책");
-            book.setAuthor("조영호");
-            book.setPrice(30000);
-            em.persist(book);
+            String memberNameA = em.createQuery("SELECT coalesce(m.name, '이름 없는 회원') from Member as m", String.class)
+                    .getSingleResult();
 
-            List<Member> members = em.createQuery("SELECT m FROM Member as m Where m.memberType = :memberType", Member.class)
-                    .setParameter("memberType", MemberType.ADMIN)
-                    .getResultList();
-            System.out.println("members.size() = " + members.size());
+            System.out.println("memberNameA = "+memberNameA);
 
-            List<Product> products = em.createQuery("SELECT p from Product as p Where type(p) = Book", Product.class)
-                    .getResultList();
-            System.out.println("products.size() = "+ products.size());
+            Member memberB = new Member();
+            memberB.setName("관리자");
+            memberB.setAge(70);
+            em.persist(memberB);
+
+            String memberNameB = em.createQuery("SELECT nullif(m.name, '관리자') from Member as m", String.class)
+                    .getSingleResult();
+            System.out.println("memberNameB = "+memberNameB);
+
+            List<String> ageResults = em.createQuery(
+                    "SELECT "+
+                            "case " +
+                                "when m.age <= 10 then '학생요금' "+
+                                "when m.age >= 60 then '경로요금' "+
+                                "else '일반요금' "+
+                            "end "+
+                        "FROM Member as m"
+                    , String.class).getResultList();
+
+            System.out.println("memberA age : "+ageResults.get(0));
+            System.out.println("memberB age : "+ageResults.get(1));
 
             tx.commit();
         } catch (Exception e) {
